919aa47ce4e3d1c9e79bcfb4d86ae83846035f57
DutraGames <gabrieldutraamarante@gmail.com>
2021-07-04 23:14:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const req = fetch('https://api.github.com/users/DutraGames')
+    .then(res => res.json())
+    .then((jsonbody)=>{
+        const img = document.getElementById("img")
+        img.innerHTML = `
+        <img src="` + jsonbody.avatar_url + `" alt="perfil" class="image" >
+        `
+
+        const nome = document.getElementById("nome")
+        nome.innerText = jsonbody.name
+
+        const bio = document.getElementById("bio")
+        bio.innerText = jsonbody.bio
+
+        const followers = document.getElementById("followers")
+        followers.innerText = jsonbody.followers
+
+        const following = document.getElementById("following")
+        following.innerText = jsonbody.following
+
+        const repos = document.getElementById("public_repos")
+        repos.innerText = jsonbody.public_repos
+
+    })
